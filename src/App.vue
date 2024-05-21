98903701f4ed3f09bae2d0809d1f5f1f13300a5a
unknown <abillabebrianti@gmail.com>
2024-05-21 14:15:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://tse2.mm.bing.net/th?id=OIP.4vNyzgL9up73zbjJa3v_lAHaC9&pid=Api&P=0&h=180">
+          </q-avatar>
+          SheeshShoes
+        </q-toolbar-title>
+      </q-toolbar>
+      <q-tabs align="left">
+        <q-route-tab to="/Home" label="Home" />
+        <q-route-tab to="/Shop" label="Shop" />
+        <q-route-tab to="/About" label="About" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+      <div class="q-pa-md">
+        <q-carousel 
+          v-model="slide" 
+          animated 
+          infinite 
+          swipeable 
+          height="300px"
+          class="my-carousel"
+        >
+          <q-carousel-slide v-for="(product, index) in products" :key="index" :img-src="product.image" :img-alt="product.name"></q-carousel-slide>
+        </q-carousel>
+
+        <q-card class="q-ma-md">
+          <q-card-section>
+            <div class="text-h6">Featured Products</div>
+          </q-card-section>
+
+          <q-card-section>
+            <q-list bordered separator>
+              <q-item v-for="(product, index) in products" :key="index">
+                <q-item-section avatar>
+                  <q-avatar>
+                    <img :src="product.image" :alt="product.name">
+                  </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ product.name }}</q-item-label>
+                  <q-item-label caption>{{ product.price }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-card-section>
+
+          <q-card-actions align="right">
+            <q-btn flat label="View All" @click="viewAllProducts" />
+            <q-btn flat label="Contact" @click="viewAllProducts" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://tse2.mm.bing.net/th?id=OIP.4vNyzgL9up73zbjJa3v_lAHaC9&pid=Api&P=0&h=180">
+          </q-avatar>
+          <div>SheeshShoes</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data() {
+    return {
+      slide: 0,
+      products: [
+        { name: 'Adidas', image: 'https://tse4.mm.bing.net/th?id=OIP.nygu3F6OprlJ8N9Yc9r72gHaHa&pid=Api&P=0&h=180', price: '$19.99' },
+        { name: 'Air Jordan', image: 'https://tse1.mm.bing.net/th?id=OIP.f7bs2l4T3HrpaM2hGtpnggHaHa&pid=Api&P=0&h=180', price: '$21.99' },
+        { name: 'Converse', image: 'https://tse3.mm.bing.net/th?id=OIP.k-1aD12PJI0XKPH5Ug0shgHaHa&pid=Api&P=0&h=180', price: '$14.99' },
+        { name: 'Vans', image: 'https://tse2.mm.bing.net/th?id=OIP.zqK8RZpbryMnKW1sEd1EoAHaHa&pid=Api&P=0&h=180', price: '$14.99' },
+        { name: 'Sepatu Futsal', image: 'https://tse4.mm.bing.net/th?id=OIP.9k1ck11gUCbDrKlU0u8B3AHaHa&pid=Api&P=0&h=180', price: '$10.99' }
+      ]
+    }
+  },
+  methods: {
+    viewAllProducts() {
+      // Action to view all products
+    }
+  }
+}
+</script>
+
+<style>
+.my-carousel {
+  max-width: 500px;
+  margin: auto;
+}
+</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    methods: {
+        goToAbout() {
+            this.$router.push('/about')
+        },
+    },
+}
+</script>
+
+<template>
+    <div>
+    <h5> Home Page </h5>
+    <p>Sepatu adalah salah satu jenis alas kaki (footwear) yang biasanya terdiri atas bagian-bagian sol, hak, kap, tali, dan lidah. Biasanya juga terbuat dari kanvas atau kulit yang menutupi semua bagian mulai dari jari jemari, punggung kaki hingga bagian tumit yang difungsikan untuk [1]melindungi kaki dari kotoran berupa debu, kerikil, atau bahkan lumpur dan puing.
+        Pengelompokkan sepatu biasanya dilakukan berdasarkan fungsi atau tipenya, seperti sepatu resmi (pesta), sepatu santai (kasual), sepatu dansa, sepatu olahraga, sepatu kerja, sepatu ortopedik, dan sepatu minimalis. Kata "sepatu" dalam bahasa Indonesia berasal dari bahasa Spanyol "zapatos" dan bahasa Portugis "sapato".
+<h6>Sejarah</h6>
+Sepatu pria berwarna coklat.
+Sepatu paling awal yang diketahui adalah sandal kulit sagebrush yang berasal dari sekitar 7000 atau 8000 SM,
+ ditemukan di Fort Rock Cave di negara bagian Oregon di AS pada tahun 1938.[2] Sepatu kulit tertua di dunia, terbuat dari sepotong kulit sapi yang diikat dengan tali kulit di sepanjang jahitan di bagian depan dan belakang,
+  ditemukan di kompleks gua Areni-1 di Armenia pada tahun 2008 dan diyakini berasal dari tahun 3500 SM Pada masa Mesir kuno, sepatu menjadi simbol status sosial. Orang-orang kaya dan bangsawan mengenakan sepatu yang terbuat dari bahan-bahan mewah seperti kulit buaya atau emas, sementara rakyat biasa menggunakan sepatu yang terbuat dari bahan yang lebih sederhana. Di zaman Romawi kuno, sepatu menjadi bagian penting dari militer, dengan desain yang kuat dan tahan lama untuk melindungi tentara dalam pertempuran.</p>
+<CarouselAndCard />
+    </div>
+  </template>
